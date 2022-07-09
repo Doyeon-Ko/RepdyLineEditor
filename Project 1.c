3d9ac1e140601b6eb78ca4f5fb2b684f504adbb5
Doyeon Ko <ok.doyeon@gmail.com>
2022-07-09 22:39:33 +0900

    1. Remove the while loop in the main function. 2. Remove the unnecessary recursion calling.

--- Project 1.c
+++ Project 1.c
@@ -6,7 +6,7 @@
 #define MaxLen 500
 
 int view_file(FILE* fp);
-void print_line(FILE* fp, int line_num, int total_line);
+void print_line(FILE* fp, int total_line);
 
 int main(int argc, char* argv[])   
 {
@@ -19,53 +19,21 @@ int main(int argc, char* argv[])
 	{
 		printf("인수의 개수가 부족합니다.\n 아래와 같이 입력하세요.\n");
 		printf("> 0704.exe <파일명> \n");
-		return 0;
+		return 0; 
 	}
 
 	else
 	{
 		if (file = fopen(argv[1], "r+"))
+		{
 			total_line = view_file(file);
-
+			print_line(file, total_line);
+		}
+		
 		else if (file == NULL)
 		{
 			printf("File does not exist.");
 			return 0;
-		}
-	}
-	
-	while (1)
-	{
-		printf("\n   Line Number : ");
-		scanf("%s", &line_num);
-
-		if (line_num[0] == '\\' && strlen(line_num) == 2)
-		{
-			if (line_num[1] == 'q' || line_num[1] == 'Q')
-			{
-				printf("   Quit Line Editior.\n");
-				fclose(file);
-				exit(1);
-			}
-		}
-
-		convert_linenum = atoi(line_num);
-		if (!convert_linenum)
-		{
-			printf("   Invalid Command.\n");
-			continue;
-		}
-
-		if (convert_linenum > 0 && convert_linenum <= total_line)
-		{
-			print_line(file, convert_linenum, total_line);
-			break;
-		}
-
-		else if (convert_linenum > total_line)
-		{
-			printf("\n   Out of range value.\n");
-			printf("   Enter the value between 1 and %d.\n", total_line);
 		}
 	}
 
@@ -81,61 +49,58 @@ int view_file(FILE* fp)
 		printf("%5d %s", line_num++, buffer);
 	}
 	printf("\n");
-	//int fpPosition = ftell(fp);
-	//printf("\n현재 파일 포인터의 위치 : %d", fpPosition);
-	//rewind(fp); 함수 독립성 문제, 한 함수는 하나의 기능만 독립적으로 수행하도록 함.
+
 	return (line_num - 1);
 }
 
-void print_line(FILE* fp, int linenum, int total_line)
+void print_line(FILE* fp, int total_line)
 {
 	int line = 1;
 	char arr[MaxCount];
 	char* get_string;
-	char answer[10] = "";
+	char answer[MaxLen] = "";
 	int convert_answer;
 
 	while (1)
 	{
-		get_string = fgets(arr, MaxCount, fp);
+		printf("\n   Line Number : ");
+		scanf("%s", answer);
 
-		if (line++ == linenum)
+		if (answer[0] == '\\' && strlen(answer) == 2)
 		{
-			printf("   %s", get_string);
-			while (1)
+			if (answer[1] == 'q' || answer[1] == 'Q')
 			{
-				printf("\n   Line Number : ");
-				scanf("%s", answer);
-
-				if (answer[0] == '\\' && strlen(answer) == 2)
-				{
-					if (answer[1] == 'q' || answer[1] == 'Q')
-					{
-						printf("   Quit Line Editior.\n");
-						fclose(fp);
-						exit(1);
-					}
-				}
-
-				convert_answer = atoi(answer);
-
-				if (convert_answer > 0 && convert_answer <= total_line)
-				{
-					rewind(fp);
-					print_line(fp, convert_answer, total_line);
-					break;
-				}
-
-				else if (convert_answer > total_line)
-				{
-					printf("\n   Out of range value.\n");
-					printf("   Enter the value between 1 and %d.\n", total_line);
-					continue;
-				}
-
-				else
-					printf("   Invalid Command.\n");
+				printf("   Quit Line Editior.\n");
+				fclose(fp);
+				exit(1);
 			}
 		}
+
+		convert_answer = atoi(answer);
+
+		if (convert_answer > 0 && convert_answer <= total_line)
+		{
+			rewind(fp);
+			while (1)
+			{
+				get_string = fgets(arr, MaxCount, fp);
+
+				if (line++ == convert_answer)
+				{
+					printf("   %s", get_string);
+					break;
+				}
+			}
+		}
+
+		else if (convert_answer < 0 || convert_answer > total_line)
+		{
+			printf("\n   Out of range value.\n");
+			printf("   Enter the value between 1 and %d.\n", total_line);
+			continue;
+		}
+
+		else
+			printf("   Invalid Command.\n");
 	}
 }
